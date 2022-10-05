891766c0e849601075da5a23386e142e02293aed
gm2102962 <jeongsy@gabia.com>
2022-10-06 00:19:48 +0900

    feat: 체크박스 컴포넌트 개발

---
+++ src/components/common/Checkbox/Checkbox.tsx
@@ -0,0 +1,50 @@
+import { HTMLAttributes, MouseEvent, useId } from 'react';
+
+export interface CheckboxProps extends HTMLAttributes<HTMLInputElement> {
+  isChecked?: boolean;
+  onClick?: () => void;
+  reversal?: boolean;
+}
+
+const Checkbox = ({ children, isChecked = false, onClick, reversal = false }: CheckboxProps) => {
+  const id = useId();
+
+  const handleClickEvent = (e: MouseEvent<HTMLInputElement>) => {
+    e.stopPropagation();
+    onClick?.();
+  };
+
+  return (
+    <label className="group inline-block" htmlFor={id}>
+      <span className="relative flex cursor-pointer select-none items-center p-0">
+        <span
+          className={`relative box-border inline-block h-[24px] w-[24px] rounded align-middle
+          ${
+            reversal
+              ? 'after:absolute after:left-[4px] after:top-[5px] after:box-content after:h-[7px] after:w-[12px] after:border-[3px] after:border-r-0 after:border-t-0 after:border-solid after:border-[#C1C7CD] after:-rotate-45 after:content-[""]'
+              : 'bg-[#C1C7CD]'
+          }
+            ${
+              isChecked
+                ? `${
+                    reversal ? 'bg-white' : '!bg-[#4394F0]'
+                  } after:absolute after:left-[4px] after:top-[5px] after:box-content after:h-[7px] after:w-[12px] after:border-[3px] after:border-r-0 after:border-t-0 after:border-solid ${
+                    reversal ? 'after:border-[#4394F0]' : 'after:border-white'
+                  } after:-rotate-45 after:content-[""]`
+                : ''
+            }
+          `}
+        />
+        <input
+          className="absolute z-[-1] m-0 h-0 w-0 opacity-0 outline-none"
+          id={id}
+          type="checkbox"
+          onClick={handleClickEvent}
+        />
+        {children || null}
+      </span>
+    </label>
+  );
+};
+
+export default Checkbox;
